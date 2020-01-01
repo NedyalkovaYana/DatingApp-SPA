d5d2783ac8cfa88edd63825336c96981629d6f12
Yanka Nedyalkova <nedyalkova.yana@gmail.com>
2020-01-01 22:35:52 +0200

    Added the 'Likes' functionality

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams, HttpErrorResponse } from '@angular/common/http';
 import { User } from '../_models/user';
 import { PaginatedResult } from '../_models/pagination';
 import { map } from 'rxjs/operators';
-
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +15,7 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  getUsers(page?, itemsPerPage?, userParams?): Observable<PaginatedResult<User[]>> {
+  getUsers(page?, itemsPerPage?, userParams?, likesParam?): Observable<PaginatedResult<User[]>> {
     const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<User[]>();
     let params = new HttpParams();
 
@@ -29,6 +29,14 @@ export class UserService {
       params = params.append('maxAge', userParams.maxAge);
       params = params.append('gender', userParams.gender);
       params = params.append('orderBy', userParams.orderBy);
+    }
+
+    if (likesParam === 'Likers') {
+      params = params.append('likers', 'true');
+    }
+
+    if (likesParam === 'Likees') {
+      params = params.append('likees', 'true');
     }
 
     return this.http.get<User[]>(this.baseUrl + 'users', {observe: 'response', params})
@@ -58,4 +66,9 @@ export class UserService {
   deletePhoto(userId: number, id: number) {
     return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + id);
   }
+
+  sendLike(id: number, recipientId: number) {
+    return this.http.post(this.baseUrl + 'users/' + id + '/like/' + recipientId, {});
+  }
+
 }
--- src/app/members/member-card/member-card.component.ts
+++ src/app/members/member-card/member-card.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { UserService } from 'src/app/_services/user.service';
 import { User } from 'src/app/_models/user';
+import { AuthService } from 'src/app/_services/auth.service';
+import { AlerifyService } from 'src/app/_services/alerify.service';
 
 @Component({
   selector: 'app-member-card',
@@ -9,9 +11,18 @@ import { User } from 'src/app/_models/user';
 })
 export class MemberCardComponent implements OnInit {
   @Input() user: User;
-  constructor() { }
+  constructor(private authService: AuthService, private userService: UserService,
+              private alertify: AlerifyService) { }
 
   ngOnInit() {
   }
 
+  sendLike(id: number) {
+    const liker: number = this.authService.decodedToken.nameid;
+    this.userService.sendLike(liker, id).subscribe(data => {
+      this.alertify.success('You have liked: ' + this.user.knownAs);
+    }, (error) => {
+      this.alertify.error(error);
+    });
+  }
 }
